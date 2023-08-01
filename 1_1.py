d99f68336698a91df577673271925a40c29a5236
rivitch <136101600+rivitch@users.noreply.github.com>
2023-08-01 19:56:41 +0400

    1

---
+++ 1_1.py
@@ -0,0 +1,48 @@
+#print_operation_table(operation, num_rows=6, num_columns=6)
+
+
+# num_rows=6
+# num_columns=6
+# def print_operation_table(y: num_rows=6, num_columns=6):
+b = x = y = ""
+y = input(b)
+
+if y == "print_operation_table(lambda x, y: x * y)":
+    num_rows=int(x)
+    num_columns=int(y)
+    for i in range(num_rows):
+        for j in range(num_columns):
+            #if (i or j) != 0:
+            x = "" 
+            if j>=1 and a>=10: # and (len(str(a)==2)):
+                x = " "
+            a = (i+1)*(j+1)
+            print(a, x, end = ' ')
+        print()
+# b =""
+# y = input(b)
+# if y == "print_operation_table(lambda x, y: x * y)":
+#     print_operation_table(y, num_rows=6, num_columns=6)
+#     print("")
+
+
+# #print_operation_table(lambda x, y: x * y)":
+# num_rows=int(6)
+# num_columns=int(6)
+# for i in range(num_rows):
+#     for j in range(num_columns):
+#         # x = "" 
+#         # if j>=1 and a>=10: # and (len(str(a)==2)):
+#         #     x = " "
+#         a = (i+1)*(j+1)
+#         print(a, end = ' ')
+#     print()
+
+
+ # x = "" 
+# if j>=1 and a>=10: # and (len(str(a)==2)):
+#     x = " "
+# строка № 1 :лот-херачил-по-волне
+# строка № 2 :в-толстой-лодке-и-с-веслой
+# строка № 3 :как-увидел-арарат(ежа)
+# строка № 4 :пошел-спать-надоело
---
+++ Sem7_T36.py
@@ -0,0 +1,7 @@
+
+num_rows=int(6)
+num_columns=int(6)
+for i in range(num_rows):
+    for j in range(num_columns):
+        print((i+1)*(j+1), end = ' ')
+    print()
